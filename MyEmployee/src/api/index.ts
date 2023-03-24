0d18932451e759e6e9bcacc743755f7ec84ba146
AkashNeoSoft <akash.yadav@neosoftmail.com>
2023-03-24 09:45:43 +0530

    env Added.

--- MyEmployee/src/api/index.ts
+++ MyEmployee/src/api/index.ts
@@ -1,24 +1,29 @@
-const URL = 'https://dummy.restapiexample.com/api/v1/';
-//const URL = BASE_URL;
+import {BASE_URL} from '@env';
 
 export const API = {
   getEmployee: (responsecallback: any) => {
-    request(responsecallback, {}, 'GET', URL + 'employees', buildHeader());
+    request(responsecallback, {}, 'GET', BASE_URL + 'employees', buildHeader());
   },
   getEmployeeById: (responsecallback: any, id: string) => {
     request(
       responsecallback,
       {},
       'GET',
-      URL + `employees/${id}`,
+      BASE_URL + `employees/${id}`,
       buildHeader(),
     );
   },
   AddEmployee: (responsecallback: any, data: any) => {
-    request(responsecallback, data, 'POST', URL + 'create', buildHeader());
+    request(responsecallback, data, 'POST', BASE_URL + 'create', buildHeader());
   },
   UpdateEmployee: (responsecallback: any, data: any, id: string) => {
-    request(responsecallback, data, 'PUT', URL + `update/${id}`, buildHeader());
+    request(
+      responsecallback,
+      data,
+      'PUT',
+      BASE_URL + `update/${id}`,
+      buildHeader(),
+    );
   },
 };
 
