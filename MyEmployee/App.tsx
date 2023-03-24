fb40c54b302210885fb066fa5258dcff19886966
AkashNeoSoft <akash.yadav@neosoftmail.com>
2023-03-24 10:29:05 +0530

    Added Flash Screen

--- MyEmployee/App.tsx
+++ MyEmployee/App.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import RootNavigator from './src/navigators/index';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return <RootNavigator />;
 }
 
